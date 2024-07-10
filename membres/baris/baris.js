77c5e914d7112c5c3ed37397c48ec87a698bbdeb
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-07-10 15:30:37 +0200

    sectionss

--- membres/baris/baris.js
+++ membres/baris/baris.js
@@ -1,7 +1,10 @@
-$('.carousel').carousel({
-    interval: 0
+$(document).ready(function() {
+    $('form').submit(function(event) {
+      event.preventDefault(); 
+      var $submitBtn = $(this).find('button[type="submit"]');
+      $submitBtn.addClass('btn-spinning');
+      setTimeout(function() {
+        $submitBtn.text('Merci !');
+      }, 1000); 
+    });
   });
-  $('.btn-next').click(function(){
-    $('.carousel').carousel('next');
-  });
-  
