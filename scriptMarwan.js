242a618298bc047e58b313e6e45470394215b93e
Sinbad212 <benmhmarwan@gmail.com>
2024-07-11 10:47:33 +0200

    2

---
+++ scriptMarwan.js
@@ -0,0 +1,159 @@
+
+AOS.init();
+       
+document.addEventListener('DOMContentLoaded', function() {
+  AOS.init();
+
+  // Attachez un événement pour déclencher la rotation
+  document.querySelector('#chefvie').addEventListener('aos:in', function() {
+    document.querySelector('#chefviepetit').classList.add('rotate');
+  });
+});
+
+$(document).ready(function(){
+    $("#MarCuillere").mouseover(function(){
+      $("#MarCuillere").css("font-size", "x-large");
+    });
+    $("#MarCuillere").mouseout(function(){
+      $("#MarCuillere").css("font-size", "20px");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#Marlegume").mouseover(function(){
+      $("#Marlegume").css("font-size", "x-large");
+    });
+    $("#Marlegume").mouseout(function(){
+      $("#Marlegume").css("font-size", "20px");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#Marboeuf").mouseover(function(){
+      $("#Marboeuf").css("font-size", "x-large");
+    });
+    $("#Marboeuf").mouseout(function(){
+      $("#Marboeuf").css("font-size", "20px");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#Marpate").mouseover(function(){
+      $("#Marpate").css("font-size", "x-large");
+    });
+    $("#Marpate").mouseout(function(){
+      $("#Marpate").css("font-size", "20px");
+    });
+  });
+  $(document).ready(function(){
+    $("#Marchine").mouseover(function(){
+      $("#Marchine").css("font-size", "x-large");
+    });
+    $("#Marchine").mouseout(function(){
+      $("#Marchine").css("font-size", "20px");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#Marsoja").mouseover(function(){
+      $("#Marsoja").css("font-size", "x-large");
+    });
+    $("#Marsoja").mouseout(function(){
+      $("#Marsoja").css("font-size", "20px");
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("#MarOignon").mouseover(function(){
+      $("#MarOignon").css("font-size", "x-large");
+    });
+    $("#MarOignon").mouseout(function(){
+      $("#MarOignon").css("font-size", "20px");
+    });
+  });
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#NouillesBoeuf").addClass("fadeInLeft");
+});
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#TextNouilles").addClass("fadeInRight");
+});
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#pexel2").addClass("fadeInRight");
+});
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#TimeNouilles").addClass("fadeInRightT");
+});
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#IngredientsMar").addClass("fadeInRightTT");
+});
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#MarCuillere").addClass("fadeInRightTTT");
+});
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#MarOignon").addClass("fadeInRightTTT");
+});
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#Marlegume").addClass("fadeInRightTTTT");
+});
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#Marboeuf").addClass("fadeInRightTTTTT");
+});
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#Marpate").addClass("fadeInRightTTTTTT");
+});
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#Marchine").addClass("fadeInRightTTTTTTT");
+});
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#Marsoja").addClass("fadeInRieght");
+});
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#TempsMar").fadeIn(2000);
+});
+
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#TextRand").addClass("fadeInLeft");
+});
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#Rand").addClass("fadeInRight");
+});
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#chefMarwan").addClass("fadeInRiieght");
+});
+
+$(document).ready(function() {
+    // Apply the fadeInLeft animation to the camping image
+    $("#chefvie").addClass("fadeInLeft");
+});
+
+$(document).ready(function(){
+    $("#MarwanAsia").scroll(function(){
+      $("#chefvie").fadeIn();
+    });
+  });
